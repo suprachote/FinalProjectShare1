bb62602839084f6253590c7f48bdfd2c70de11e2
kakaxe <suprachote@gmail.com>
2016-04-29 23:09:04 +0700

    first upload

--- app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/ui/camera/GraphicOverlay.java
+++ app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/ui/camera/GraphicOverlay.java
@@ -26,7 +26,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * A view which renders a series of custom graphics to be overlayed on top of an associated preview
+ * A view which renders a series of  custom graphics to be overlayed on top of an associated preview
  * (i.e., the camera preview).  The creator can add graphics objects, update the objects, and remove
  * them, triggering the appropriate drawing and invalidation within the view.<p>
  *
